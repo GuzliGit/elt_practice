8fd0b227b4708a7c58952bca0b548669b983429c
GuzliGit <badindan2003@gmail.com>
2025-08-08 11:40:24 +0000

    Добавление передачи файла в 13

--- Module3/13/tcp_client.c
+++ Module3/13/tcp_client.c
@@ -1,3 +1,5 @@
+#include <linux/limits.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <netinet/in.h>
@@ -6,8 +8,13 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
 
 #define BUF_SIZE 1024
+
+static int my_sock;
 
 void error(char* msg)
 {
@@ -15,9 +22,89 @@ void error(char* msg)
     exit(EXIT_FAILURE);
 }
 
+void terminate(int sig)
+{
+    close(my_sock);
+    exit(EXIT_SUCCESS);
+}
+
+void send_file(int sock, const char* filename) 
+{
+    struct stat file_stat;
+    if (stat(filename, &file_stat) < 0) 
+    {
+        printf("ERROR could not get file size for %s\n", filename);
+        return;
+    }
+    long filesize = file_stat.st_size;
+
+    int file_fd = open(filename, O_RDONLY);
+    if (file_fd < 0) 
+    {
+        printf("ERROR could not open file %s: %s\n", filename, strerror(errno));
+        return;
+    }
+
+    char command[BUF_SIZE];
+    snprintf(command, sizeof(command), "upload %s\n", filename);
+    if (send(sock, command, strlen(command), 0) < 0) 
+    {
+        perror("send upload command");
+        close(file_fd);
+        return;
+    }
+
+    char ready[7];
+    if (recv(sock, ready, 7, 0) < 3) 
+    {
+        printf("Unknown upload\n");
+        close(file_fd);
+        return;
+    }
+    ready[6] = '\0';
+
+    if (strcmp(ready, "READY") != 0) 
+    {
+        printf("server refuse upload\n");
+        close(file_fd);
+        return;
+    }
+
+    char size_str[64];
+    snprintf(size_str, sizeof(size_str), "%ld", filesize);
+    if (send(sock, size_str, strlen(size_str), 0) < 0) 
+    {
+        perror("send file size");
+        close(file_fd);
+        return;
+    }
+
+    char buffer[BUF_SIZE];
+    ssize_t bytes_read;
+    size_t total_bytes = 0;
+    
+    printf("Uploading %s...\n", filename);
+    
+    while ((bytes_read = read(file_fd, buffer, BUF_SIZE)) > 0) 
+    {
+        if (send(sock, buffer, bytes_read, 0) < 0) 
+        {
+            perror("send file data");
+            break;
+        }
+
+        total_bytes += bytes_read;
+        printf("\rSent: %zu bytes", total_bytes);
+        fflush(stdout);
+    }
+
+    printf("\n");
+    close(file_fd);
+}
+
 int main(int argc, char* argv[])
 {
-    int my_sock, portno, n;
+    int portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
     char buff[BUF_SIZE];
@@ -29,6 +116,9 @@ int main(int argc, char* argv[])
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+
+    signal(SIGINT, terminate);
+    signal(SIGTERM, terminate);
 
     portno = atoi(argv[2]);
     my_sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -63,6 +153,16 @@ int main(int argc, char* argv[])
 
         printf("S<=C:");
         fgets(&buff[0], sizeof(buff) - 1, stdin);
+
+        if (strncmp(buff, "upload ", 7) == 0) 
+        {
+            char filename[PATH_MAX];
+            strncpy(filename, buff + 7, BUF_SIZE);
+            filename[strcspn(filename, "\n")] = '\0';
+            
+            send_file(my_sock, filename);
+            continue;
+        }
 
         if (!strcmp(&buff[0], "quit\n")) 
         {
--- Module3/13/tcp_server.c
+++ Module3/13/tcp_server.c
@@ -1,4 +1,6 @@
 #include "calc/calc.h"
+#include <fcntl.h>
+#include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
@@ -9,12 +11,15 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
-#define BUF_SIZE 1024
+#define BUF_SIZE 64000
+#define FILES_DIR "server_files"
 
 static int sockfd;
 static int nclients = 0;
 
+void upload_file(int, const char*);
 void dostuff(int);
 
 void error(char* msg)
@@ -52,6 +57,15 @@ void stop_handler(int sig)
     exit(EXIT_SUCCESS);
 }
 
+void create_files_dir()
+{
+    struct stat st = {0};
+    if (stat(FILES_DIR, &st) == -1)
+    {
+        mkdir(FILES_DIR, 0700);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     char buff[BUF_SIZE];
@@ -67,6 +81,8 @@ int main(int argc, char *argv[])
         fprintf(stderr, "ERROR, no port provided\n");
         exit(EXIT_FAILURE);
     }
+
+    create_files_dir();
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
@@ -130,6 +146,77 @@ int main(int argc, char *argv[])
     return EXIT_SUCCESS;
 }
 
+void upload_file(int sock, const char* filename)
+{
+    const char *cor_name = strrchr(filename, '/');
+    if (cor_name == NULL) 
+    {
+        cor_name = filename;
+    } 
+    else 
+    {
+        cor_name++;
+    }
+
+    char filepath[PATH_MAX];
+    snprintf(filepath, sizeof(filepath), "%s/%s", FILES_DIR, cor_name);
+    
+    int file_fd = open(filepath, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if (file_fd < 0) 
+    {
+        char error_msg[BUF_SIZE];
+        snprintf(error_msg, sizeof(error_msg), "ERROR could not create file %s\n", cor_name);
+        write(sock, error_msg, strlen(error_msg));
+        return;
+    }
+    
+    char buffer[BUF_SIZE];
+    ssize_t bytes_received;
+    size_t total_bytes = 0;
+    
+    write(sock, "READY", 5);
+    
+    char size_str[64] = {0};
+    int n = read(sock, size_str, sizeof(size_str)-1);
+    if (n <= 0) 
+    {
+        close(file_fd);
+        return;
+    }
+    long filesize = atol(size_str);
+
+    while (total_bytes < (size_t)filesize && (bytes_received = read(sock, buffer, BUF_SIZE)) > 0) 
+    {
+        if (bytes_received < 0) 
+        {
+            perror("read file");
+            break;
+        }
+        
+        ssize_t bytes_written = write(file_fd, buffer, bytes_received);
+        if (bytes_written != bytes_received) 
+        {
+            perror("write file");
+            break;
+        }
+        
+        total_bytes += bytes_received;
+    }
+    
+    close(file_fd);
+    
+    if (bytes_received < 0) 
+    {
+        perror("read file");
+        unlink(filepath);
+        return;
+    }
+
+    char success_msg[BUF_SIZE];
+    snprintf(success_msg, sizeof(success_msg), "File %s uploaded successfully (%zu bytes)\n", cor_name, total_bytes);
+    write(sock, success_msg, strlen(success_msg));
+}
+
 void dostuff (int sock) {
     int bytes_recv;
     char buff[20 * BUF_SIZE];
@@ -143,6 +230,30 @@ void dostuff (int sock) {
         if (bytes_recv < 0)
         {
             error("ERROR reading from socket\n");
+        }
+
+        if (strncmp(buff, "upload", strlen("upload")) == 0) 
+        {
+            char filename[PATH_MAX];
+            char *space = strchr(buff, ' ');
+            
+            if (space != NULL) 
+            {
+                strncpy(filename, space + 1, sizeof(filename)-1);
+                filename[sizeof(filename)-1] = '\0';
+                
+                char *newline = strchr(filename, '\n');
+                if (newline) *newline = '\0';
+                
+                upload_file(sock, filename);
+            } 
+            else 
+            {
+                write(sock, "ERROR invalid upload command format. Use: upload <filename>\n", 58);
+            }
+            
+            memset(buff, 0, sizeof(buff));
+            continue;
         }
 
         int new_l = 0;
